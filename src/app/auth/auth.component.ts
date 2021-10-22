da7ed2ae3c7c4c5d9f35a440e99d0d7045c647be
dravog7 <dravog77@gmail.com>
2021-10-22 17:51:08 +0530

    login done

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, UserCredentials } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,19 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-  isLogin: boolean=false;
+  isLogin: boolean = true;
+  isLogged: boolean = false;
+  userCred = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+  }
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
-  Login()
-  {
-    this.isLogin=true;
-  }
-  Signup()
-  {
-    this.isLogin=false;
-  }
 
+  onLogin(){
+    this.auth.login(
+      new UserCredentials(this.userCred.username,this.userCred.password)
+      )?.subscribe(
+        isLogged=>{
+          // add navigation to /admin and /user here
+          this.isLogged = isLogged;
+        }
+        )
+  }
 }
