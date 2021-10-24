fcb1b0ade1d22e0400868248032a1e0f594bec9a
John Abraham <31211464+dravog7@users.noreply.github.com>
2021-10-24 20:56:31 +0530

    added BookDetails and readded old navbar with fixes (#6)
    * authentication service setup
    
    * login done
    
    * registration fixed, ng-bootstrap installed
    
    * user/ categories component
    
    * localStorage auth, Category to Book routing
    
    * books setup
    
    * fixed nav bar
    
    * navbar and basic card fixes
    
    * book details popup
    
    * min 2 per row card decks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book, BookService } from '../shared/services/book.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  bookList: Book[] = []
 
-  constructor() { }
+  constructor(private books: BookService) { }
 
   ngOnInit(): void {
+    this.books.getList({ featured: true }).subscribe(
+      value => this.bookList = value
+    )
   }
-
 }
